a428feb657d0cebc2e58051f0fced9cb4a3160ca
Rade <rade.pilkinton@wsu.edu>
2020-08-26 12:05:14 -0700

    minor styling

--- src/components/ChatTile.jsx
+++ src/components/ChatTile.jsx
@@ -4,18 +4,34 @@ import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles({
     tile: {
         width: "100%",
-        height: "auto",
-        backgroundColor: "grey",
-        borderBottom: "1px solid white"
+        minHeight: "40px",
+        borderBottom: "1px solid rgba(255,255,255,0.3)",
+        overflow: "hidden",
+        display: "flex"
     },
+    author: {
+        width: "20%",
+        backgroundColor: "rgba(255,255,255,0.15)",
+        alignItems: "stretch",
+        lineHeight: "3",
+        textAlign: "center"
+    },
+    message: {
+        width: "80%",
+        backgroundColor: "rgba(255,255,255,0.1)",
+        alignItems: "stretch",
+        lineHeight: "3",
+        color: "white",
+        paddingLeft: "10px"
+    }
 });
 
 function ChatTile (props) {
     const classes = useStyles();
     return (
         <div className={classes.tile}>
-            <div className="author">{props.author}</div>
-            <div className="message">{props.message}</div>
+            <div className={classes.author}>{props.author}</div>
+            <div className={classes.message}>{props.message}</div>
         </div>
     )
 }
--- src/views/TwitchChatClient.jsx
+++ src/views/TwitchChatClient.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
 import ChatStream from "../components/ChatStream";
 
+const useStyles = makeStyles({
+    appcontainer: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#121212",
+        overflow: "auto"
+    },
+});
+
 function TwitchChatClient () {
-    return <ChatStream/>;
+    const classes = useStyles();
+    return (
+        <div className={classes.appcontainer}>
+            <ChatStream/>
+        </div>
+    );
 }
 
 export default TwitchChatClient;
